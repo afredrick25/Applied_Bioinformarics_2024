857555a0e6dae831bf0b685e735840cde305f093
afredrick25 <afredrick@claflin.edu>
2024-09-09 09:38:25 -0400

    Add files via upload

--- First_Program.R
+++ First_Program.R
@@ -9,3 +9,11 @@ data <- ReadAffy()
 
 # Plot the dataset
 boxplot(data,  xlab="Samples", ylab="Genes", main="Raw Micrarrary Plot", col="blue")
+
+# perform normalizatio on raw datasets
+data_norm <- rma(data)
+
+data_norm <- exprs(data_norm)
+
+#plot normalized dataset
+boxplot(data_norm,  xlab="Samples", ylab="Genes", main="Normalized Micrarrary Plot", col="green")
