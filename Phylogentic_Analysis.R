585dbeeaaac6e0a247c841cda0cb944f5823d692
afredrick25 <afredrick@claflin.edu>
2024-09-18 09:46:10 -0400

    Add files via upload

---
+++ Phylogentic_Analysis.R
@@ -0,0 +1,7 @@
+#Author: Anaiya; Date: September 18, 2024; Purpose: Create dummy data, generate phylogenetic tree plot
+
+#load tree libraries
+library(ape)
+
+#generate some dummy data
+text.string <- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark);"
