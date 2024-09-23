cd4a52f4f07707ed4cf348f671e45fc36d04bfae
afredrick25 <afredrick@claflin.edu>
2024-09-23 09:44:42 -0400

    Add files via upload

--- Phylogentic_Analysis.R
+++ Phylogentic_Analysis.R
@@ -5,3 +5,9 @@ library(ape)
 
 #generate some dummy data
 text.string <- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark);"
+
+#read the dummy data as a tree structure
+vert.tree<-read.tree(text=text.string)
+
+#genersate a basic plot of dummy data
+plot(vert.tree,no.margin=TRUE,edge.width=2)
